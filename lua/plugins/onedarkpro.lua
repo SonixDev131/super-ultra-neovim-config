995cb1ec1355f1400f06ed360b6b7424473b05c4
SonixDev131 <son.nl215131@gmail.com>
2025-06-28 14:09:26 +0000

    add leetcode, linting, formatting and tmux-navigator

--- lua/plugins/onedarkpro.lua
+++ lua/plugins/onedarkpro.lua
@@ -1,15 +1,21 @@
 return {
-  "olimorris/onedarkpro.nvim",
-  priority = 1000, -- Ensure it loads first
-  config = function()
-    require("onedarkpro").setup({
-      options = {
-        cursorline = true,
-        transparency = true,
-        lualine_transparency = true,
-      },
-    })
-
-    vim.cmd("colorscheme onedark")
-  end,
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
+    config = function()
+      require("onedarkpro").setup({
+        options = {
+          cursorline = true,
+          transparency = true,
+          lualine_transparency = true,
+        },
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
 }
